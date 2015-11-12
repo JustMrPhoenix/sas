ba26b2f8872b24beb522ef8e2970cd9a8046ef31
Антон Истомин <istomanton@gmail.com>
2015-11-12 22:37:08 +0500

    remove conflicted package my

--- src/my/sas/message/EnglishMessage.java
+++
@@ -1,6 +0,0 @@
-package my.sas.message;
-
-public class EnglishMessage {
-    public static final String YES = "yes";
-    public static final String NO = "no";
-}
---
+++ src/test/java/mrphoenix/sas/geom/DiskTest.java
@@ -0,0 +1,12 @@
+package mrphoenix.sas.geom;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DiskTest {
+
+    @Test
+    public void testGetRandomPoint() throws Exception {
+        Assert.assertTrue(true);
+    }
+}
